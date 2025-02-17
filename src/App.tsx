1121efbef5504d90f8d416e555f14a59a48b8e57
Valerii Viun <2455521@gmail.com>
2025-02-17 14:43:52 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import reactLogo from "./assets/react.svg";
+// import RecipesOldWay from "./components/recipesOldWay";
+import RecipesActionsWay from "./components/recipesActionsWay";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 style={{ marginTop: 0, marginBottom: "18px" }}>React 19</h1>
+      <h2 style={{ margin: 0 }}>
+        {/* <a
+          href="https://www.youtube.com/@SimpleFrontendUA"
+          target="_blank"
+          style={{ color: "#E464C1" }}
+        > 
+          Простий Фронтенд
+        </a> */}
+      </h2>
+      <div className="card">
+        {/* <RecipesOldWay /> */}
+        <RecipesActionsWay />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/recipesActionsWay.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect, useTransition } from "react";
+
+type Recipe = {
+  id: number;
+  name: string;
+  image: string;
+};
+
+const RecipesActionsWay = () => {
+  const [recipes, setRecipes] = useState<Recipe[] | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [isPending, startTransition] = useTransition();
+
+  const fetchRecipes = () => {
+    startTransition(async () => {
+      const response = await fetch("https://dummyjson.com/recipes");
+      if (!response.ok) {
+        setError(new Error(`Error: ${response.status} ${response.statusText}`));
+        return;
+      }
+      const data = await response.json();
+      setRecipes(data.recipes);
+    });
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+  return (
+    <div>
+      <h2 style={{ margin: 0 }}>Recipes</h2>
+      {isPending && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}> {error.message} </p>}
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        {recipes?.map((recipe) => (
+          <li key={recipe.id}>
+            <i>{recipe.name} </i>
+            <div>
+              <img
+                style={{ width: "180px", margin: "12px 0 24px 0" }}
+                src={recipe.image}
+                alt={recipe.name}
+              />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecipesActionsWay;
